46b483546f14c2d32f074e1779bdf5c611ba8c47
vinay <vk.vinaykumar9793@gmail.com>
2026-01-02 14:03:39 +0530

    header file completed

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter , Routes , Route } from "react-router-dom";
 import SignUp from "./pages/SignUp";
 import SignIn from "./pages/SignIn";
 import Home from "./pages/Home";
+import Header from "./components/Header";
 // import About from "./pages/About";
 // import Search from "./pages/Search";
 // import Listing from "./pages/Listing";
@@ -10,24 +11,10 @@ import Home from "./pages/Home";
 export default function App(){
   return(
   <BrowserRouter>
-      
+     <Header />
         <Routes>
 
-          {/* <Route path='/' element={<Home />} />
-          <Route path='/sign-in' element={<SignIn />} />
-          <Route path='/sign-up' element={<SignUp />} />
-          <Route path='/about' element={<About />} />
-          <Route path='/search' element={<Search />} />
-          <Route path='/listing/:listingId' element={<Listing />} />
-  
-          <Route element={<PrivateRoute />}>
-            <Route path='/profile' element={<Profile />} />
-            <Route path='/create-listing' element={<CreateListing />} />
-            <Route
-              path='/update-listing/:listingId'
-              element={<UpdateListing />}
-            />
-          </Route> */}
+           
           <Route path='/'  element={<Home />} />
           <Route path='/sign-in'  element={<SignIn />} />
           <Route path='/sign-up'  element={<SignUp />} />
